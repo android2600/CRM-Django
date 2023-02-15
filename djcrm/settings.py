35d3843d0298ebdab5d50db711ddfa6aaec9c03f
android2600 <95949373+android2600@users.noreply.github.com>
2023-02-16 01:00:04 +0800

    update

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -1,10 +1,14 @@
 from pathlib import Path
-# import environ
+import environ
 import os
+env= environ.Env(
+    DEBUG=(bool,False)
+)
+environ.Env.read_env()
 
-DEBUG = True
+DEBUG = env("DEBUG")
 
-SECRET_KEY="django-insecure-+xc=4h4-=nkl6t(=4j@a04wi3e4qqt7k+h$sd4%^4nt4j(2pqm" #env('SECRET_KEY')
+SECRET_KEY=env('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,11 +74,14 @@ WSGI_APPLICATION = "djcrm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER":env("DB_USER"),
+        "PASSWORD":env("DB_PASSWORD"),
+        "HOST":env("DB_HOST"),
+        "PORT":env("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -111,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS=os.path.join(BASE_DIR,"static/") # For static files
+STATICFILES_DIRS=os.path.join(BASE_DIR,"static/"), # For static files
 STATIC_ROOT= os.path.join(BASE_DIR,"staticfiles_build","static")  #For static Files
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
